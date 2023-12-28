5fb05f687b1e10cf1d441761e2072bd517f2e066
Priyanka <138300397+sumapigit@users.noreply.github.com>
2023-12-28 22:49:26 +0530

    Add files via upload

---
+++ colourswitch.js
@@ -0,0 +1,25 @@
+// select buttons
+const buttons=document.querySelectorAll('.button')
+
+//select body
+const body=document.querySelector('body')
+
+buttons.forEach(function(button){
+    console.log(button)
+    button.addEventListener('click',function(e){
+        console.log(e)
+        console.log(e.target)
+        if(e.target.id==='pink'){
+            body.style.backgroundColor=e.target.id
+        }
+        if(e.target.id==='yellow'){
+            body.style.backgroundColor=e.target.id
+        }
+        if(e.target.id==='blue'){
+            body.style.backgroundColor=e.target.id
+        }
+        if(e.target.id==='orange'){
+            body.style.backgroundColor=e.target.id
+        }
+    })
+})
